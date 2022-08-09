b637cf789b8fa4172303aa3880ee19a569f2445f
abdul aziz <azizkrian@gmail.com>
2022-08-09 20:33:53 +0700

    push progress final project

--- kalkulator/kalkulator.js
+++ kalkulator/kalkulator.js
@@ -8,17 +8,18 @@ const kalkulator = {
 function updateDisplay() {
     document.querySelector('#displayNumber').innerText = kalkulator.displayNumber;
 }
-console.log(kalkulator.displayNumber);
+updateDisplay()
 
-function clearKalkulator() {
-    kalkulator.displayNumber = '0',
-        kalkulator.operator = null,
-        kalkulator.firstNumber = null,
-        kalkulator.isWaitForSecondNumber = false
-}
+
+
 
 function inputDigit(digit) {
-    kalkulator.displayNumber += digit
+    if (kalkulator.displayNumber === '0') {
+        kalkulator.displayNumber = digit;
+    } else {
+        kalkulator.displayNumber += digit;
+    }
+
 }
 
 const buttons = document.querySelectorAll('.button')
@@ -28,9 +29,83 @@ for (const button of buttons) {
         const target = event.target;
         console.log(target);
 
-        inputDigit(target.innerText)
+        if (target.classList.contains('clearr')) {
+            clearKalkulator();
+            updateDisplay();
+            return
+        }
+        function clearKalkulator() {
+            kalkulator.displayNumber = '0';
+            kalkulator.operator = null;
+            kalkulator.firstNumber = null;
+            kalkulator.isWaitForSecondNumber = false;
+        }
+        if (target.classList.contains('negative')) {
+            negativeNumber();
+            updateDisplay();
+            return
+        }
+        function negativeNumber() {
+            if (kalkulator.displayNumber === '0') {
+                return;
+            } else {
+                kalkulator.displayNumber *= - 1;
+            }
+        }
+        if (target.classList.contains('operator')) {
+
+            operatorNumber(target.innerHTML);
+
+            updateDisplay();
+            return
+        }
+        function operatorNumber(operator) {
+            if (!kalkulator.isWaitForSecondNumber) {
+                kalkulator.operator = operator;
+                kalkulator.isWaitForSecondNumber = true;
+                kalkulator.firstNumber = kalkulator.displayNumber;
+                kalkulator.displayNumber = '';
+            }
+            else {
+                alert('operator sudah ditetapkan')
+            }
+        }
+        if (target.classList.contains('equals')) {
+            equalsButton();
+            updateDisplay();
+            return
+        }
+        function equalsButton() {
+            if (kalkulator.firstNumber == null || kalkulator.operator == null) {
+                return;
+            }
+            let result = 0;
+            if (kalkulator.operator == '+') {
+                result = parseInt(kalkulator.firstNumber) + parseInt(kalkulator.displayNumber)
+            } else {
+                result = parseInt(kalkulator.firstNumber) - parseInt(kalkulator.displayNumber)
+            }
+            const history = {
+                firstNumber: kalkulator.firstNumber,
+                secondNumber: kalkulator.displayNumber,
+                operator: kalkulator.operator,
+                result: result
+            }
+            putHistory(history)
+            kalkulator.displayNumber = result
+            renderHistory()
+
+
+        }
+
+
+
+        inputDigit(target.innerHTML)
         updateDisplay();
+
+
     })
+
 
 }
 
